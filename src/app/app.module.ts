4c913f5383c759833ca57a348e39fef4d4c2a30b
Jordi1080 <jordi.hesselz@hotmail.com>
2020-12-03 17:22:26 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NameDisplayerComponent } from './name-displayer/name-displayer.component';
+import { CharactersComponent } from './characters/characters.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ConverterComponent } from './converter/converter.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
+import { GamedevComponent } from './gamedev/gamedev.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing.module';
+// import {FormsModule} from '@angular/forms'; dit toevoegen als er iets met forms niet werkt
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameDisplayerComponent,
+    CharactersComponent,
+    NavbarComponent,
+    ConverterComponent,
+    GalleryComponent,
+    HomeComponent,
+    GamedevComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
+    , FormsModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
